0240e2ef59d283d3cd069598023d6d9a88b59279
Joe Nilson <joenilson@gmail.com>
2017-01-25 09:43:33 -0400

    kardex: fix a generación de excel

--- controller/informe_analisisarticulos.php
+++ controller/informe_analisisarticulos.php
@@ -86,7 +86,7 @@ class informe_analisisarticulos extends fs_controller
       $basepath = dirname(dirname(dirname(__DIR__)));
       $this->documentosDir = $basepath.DIRECTORY_SEPARATOR.FS_MYDOCS.'documentos';
       $this->kardexDir = $this->documentosDir.DIRECTORY_SEPARATOR."kardex";
-      $this->publicPath = FS_MYDOCS.'documentos'.DIRECTORY_SEPARATOR.'kardex';
+      $this->publicPath = FS_PATH.FS_MYDOCS.'documentos'.DIRECTORY_SEPARATOR.'kardex';
       
       if(!is_dir($this->documentosDir)){
           mkdir($this->documentosDir);
@@ -177,7 +177,7 @@ class informe_analisisarticulos extends fs_controller
 
    public function kardex_almacen(){
       $resumen = array();
-      $this->fileName = $this->$this->publicPath.DIRECTORY_SEPARATOR.'Kardex'."_".$this->user->nick.".xlsx";
+      $this->fileName = $this->publicPath.DIRECTORY_SEPARATOR.'Kardex'."_".$this->user->nick.".xlsx";
       if(file_exists($this->fileName)){
          unlink($this->fileName);
       }
